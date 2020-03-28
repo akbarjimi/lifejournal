d8de0064f25380240c282379e09755877f1f8092
mohammad akbari <mohammad.akbari@outlook.com>
2020-03-28 20:43:50 +0430

    initial project

---
+++ database/factories/SerialFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Serial;
+use Faker\Generator as Faker;
+
+$factory->define(Serial::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
